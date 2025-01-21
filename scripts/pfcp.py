daad746e37b5fae872e94fd6b8e5adb206388178
yathy1040 <tyathusan26@gmail.com>
2025-01-21 13:17:15 -0500

    added new script (to be tested)

---
+++ scripts/pfcp.py
@@ -0,0 +1,10 @@
+from scapy.all import *
+
+target_ip = "10.10.4.1"
+target_port = 8805
+
+ip = IP(dst = target_ip)
+tcp = UDP(sport = 8805, dport = target_port)
+pfcp = PFCP(version=1, S=1, seq=1, seid=1) / PFCPSessionDeletionRequest()
+p = ip / tcp / pfcp
+sendp(p, iface="n4", loop = 0, verbose = 0)
